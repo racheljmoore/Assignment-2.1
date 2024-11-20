4135434c34e1d5761e6e014195f9bbb044531d4d
rmoore0805 <rmoore88499@uat.edu>
2024-11-19 20:39:17 -0600

    Add project files.

---
+++ Assignment 2.1/Main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "Main.h"
+
+
+static void recursiveBackwardsCounting( int startingpoint )
+{
+	if (startingpoint >> 0)
+
+
+	{
+		void recursiveBackwardsCounting(int startingpoint);
+
+		recursiveBackwardsCounting(--startingpoint);
+	}
+}
+void functionE(int* value)
+{
+	std::cout << *value << std::endl;
+	*value += 1;
+}
+
+int main(int arg, char* argv[])
+{
+	//recursiveBackwardsCounting(5);
+
+	int a = 0; //0xA678
+	
+	// a = 0
+	// &a = 0xA678
+
+	functionE(&a);//0xA678
+
+
+std::cout<< &a << std::endl;
+
+std::cout << a << std::endl; // 1
+
+//call the run application function
+//return RunApplication(argc, argv);
+
+//0
+//0x678
+//1
+
+
+
+}
